27f114c31c5b2fd10a2cc346348278e438ac5eb3
Steven M. Caruso <51963719+smcaruso@users.noreply.github.com>
2025-01-02 17:50:17 -0500

    base scene
    minmal three.js setup with orbit controls

--- src/main.js
+++ src/main.js
@@ -1,24 +1,46 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as three from "three"
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const appContainer = document.querySelector('#app')
 
-setupCounter(document.querySelector('#counter'))
+// Minimum three.js renderer, scene and camera setup
+const scene = new three.Scene()
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new three.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+appContainer.appendChild(renderer.domElement)
+
+// make renderer and canvas clear color transparent
+renderer.setClearColor(0x000000, 0)
+
+// add fog to the scene
+scene.fog = new three.Fog(0x242424, 20, 100)
+
+// event listener to auto-resize the renderer when the window is resized
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+})
+
+// add grid helper to scene
+const gridHelper = new three.GridHelper(1000, 1000, 0x444444, 0x444444)
+scene.add(gridHelper)
+
+// set up orbit controls
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.zoomToCursor = true
+camera.position.set( 20, 20, 20 )
+controls.target.set(0, 0, 0)
+controls.update()
+
+// render loop
+function animate() {
+    requestAnimationFrame(animate)
+    controls.update()
+    renderer.render(scene, camera)
+}
+
+animate()
